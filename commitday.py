039b1e340314ff0eba0b0d5c03f020714b8c6c36
root <root@localhost.localdomain>
2013-06-21 17:37:29 +0900

    Cleanup regex

--- commitday.py
+++ commitday.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     test_date, sort = util.parse_option()
     days = "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
     d = dict([(x, 0) for x in days])
-    r = re.compile(r"^(\d{4}-\d{2}-\d{2}) .+ @(\S+) ")
+    r = re.compile(r"^(\d{4}-\d{2}-\d{2}) (\S+) ")
 
-    for x in util.popen_hglog("--template", "{date|isodate} @{date|date}\n"):
+    for x in util.popen_hglog("--template", "{date|shortdate} {date|date}\n"):
         m = r.match(x)
         if m:
             date, day = m.groups()
